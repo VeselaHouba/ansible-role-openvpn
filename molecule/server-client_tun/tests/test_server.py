a17ce6446a400b5ab1f55935fbfcfbd888e69bff
Michalek <michalek@m-cloud.cz>
2021-08-09 17:15:19 +0200

    ovpn extension for client configs
    Fixes #4

--- molecule/server-client_tun/tests/test_server.py
+++ molecule/server-client_tun/tests/test_server.py
@@ -9,3 +9,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_ping_client(host):
     client = host.addr("10.8.8.2")
     assert client.is_reachable
+
+
+def test_client_configs_exists(host):
+    file1 = host.file("/etc/openvpn/master/client01")
+    file2 = host.file("/etc/openvpn/client/client01.ovpn")
+    assert file1.exists
+    assert file2.exists
